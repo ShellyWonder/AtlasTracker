f5c4dc9678317100d46196f3ca759cead0d580b5
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-03-02 09:52:36 -0600

    adding enums

---
+++ AtlasTracker/Models/Enums/BTTicketType.cs
@@ -0,0 +1,14 @@
+﻿namespace AtlasTracker.Models.Enums
+{
+    public enum BTTicketType
+    {
+        NewDevelopment,
+        WorkTask,
+        Defect,
+        ChangeRequest,
+        Enhancement,
+        GeneralTask
+           
+           
+    }
+}
