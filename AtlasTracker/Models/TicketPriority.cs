be6d84ec1ab227d3df29aa498b7802e09f92242b
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-03-01 21:38:41 -0600

    Scaffolded Controllers & views; scaffolded identity;

--- AtlasTracker/Models/TicketPriority.cs
+++ AtlasTracker/Models/TicketPriority.cs
@@ -1,6 +1,11 @@
-﻿namespace AtlasTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtlasTracker.Models
 {
     public class TicketPriority
     {
+        public int Id { get; set; }     
+        [Required]
+        public string Name { get; set; }
     }
 }
--- AtlasTracker/Program.cs
+++ AtlasTracker/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));builder.Services.AddDefaultIdentity<BTUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
