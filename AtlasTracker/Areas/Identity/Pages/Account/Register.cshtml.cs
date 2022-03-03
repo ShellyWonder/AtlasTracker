98b8c8c8ee961fbf3b28f1220c6e6d82870d7a7f
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-03-02 19:38:40 -0600

    changing registration pages

--- AtlasTracker/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AtlasTracker/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,10 +71,19 @@ namespace AtlasTracker.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            //[Required]
+            //[Display(Name = "Company Name")]
+            //public string Company { get; set; }
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -114,6 +123,9 @@ namespace AtlasTracker.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                //user.Company = Input.Company;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
