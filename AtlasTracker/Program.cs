c6f59594c74552babc4bde3657a1e2e8c47aff29
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-03-08 13:17:02 -0600

    added file service and interface

--- AtlasTracker/Program.cs
+++ AtlasTracker/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddScoped<IBTRolesService, BTRolesService>();
 builder.Services.AddScoped<IBTProjectService, BTProjectService>();
 builder.Services.AddScoped<IBTTicketService, BTTicketService>();
 builder.Services.AddScoped<IBTLookupService, BTLookupService>();
+builder.Services.AddScoped<IBTFileService, BTFileService>();
    
 
 
