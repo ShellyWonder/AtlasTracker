e44fee2f87e4504505cd0903195aa3a94060e9e0
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-03-17 00:12:56 -0500

    added new migration

--- AtlasTracker/Data/Migrations/20220316191002_0015.Designer.cs
+++ AtlasTracker/Data/Migrations/20220316191002_0015.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AtlasTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AtlasTracker.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220316191002_0015")]
+    partial class _0015
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
