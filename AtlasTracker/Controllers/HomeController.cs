9bfc35c36dc5e14e139dfad5dbb87b352ab17953
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-05-24 08:56:35 -0500

    re-implementing google charts methods to restore lost code

--- AtlasTracker/Controllers/HomeController.cs
+++ AtlasTracker/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using AtlasTracker.Extensions;
 using AtlasTracker.Models;
+using AtlasTracker.Models.Enums;
 using AtlasTracker.Models.ViewModels;
 using AtlasTracker.Services;
 using AtlasTracker.Services.Interfaces;
@@ -14,14 +15,20 @@ namespace AtlasTracker.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBTCompanyInfoService _companyInfoService;
-        
+        private readonly IBTProjectService _projectService;
+        private readonly IBTTicketService _ticketService;
 
-        public HomeController(ILogger<HomeController> logger, IBTCompanyInfoService companyInfoService)
+        public HomeController(ILogger<HomeController> logger, 
+                                    IBTCompanyInfoService companyInfoService, 
+                                    IBTProjectService projectService, 
+                                    IBTTicketService ticketService)
         {
             _logger = logger;
             _companyInfoService = companyInfoService;
+            _projectService = projectService;
+            _ticketService = ticketService;
         }
-        
+
         [HttpGet]   
         [Authorize]
         public  async Task<IActionResult> Dashboard(string swalMessage = null!)
@@ -48,6 +55,77 @@ namespace AtlasTracker.Controllers
             }
             return View();
         }
+        [HttpPost]
+        //Google Charts Implementation
+        public async Task<JsonResult> GglProjectTickets()
+        {
+            int companyId = User.Identity!.GetCompanyId();
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+            List<object> chartData = new();
+            chartData.Add(new object[] { "ProjectName", "TicketCount" });
+            foreach (Project prj in projects)
+            {
+                chartData.Add(new object[] { prj.Name!, prj.Tickets!.Count() });
+            }
+            return Json(chartData);
+        }
+        [HttpPost]
+        //Google Charts Implementation
+        public async Task<JsonResult> GglProjectPriority()
+        {
+            int companyId = User.Identity!.GetCompanyId();
+            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+            List<object> chartData = new();
+            chartData.Add(new object[] { "Priority", "Count" });
+            foreach (string priority in Enum.GetNames(typeof(BTProjectPriority)))
+            {
+                int priorityCount = (await _projectService.GetAllProjectsByPriority(companyId, priority)).Count();
+                chartData.Add(new object[] { priority, priorityCount });
+            }
+
+            return Json(chartData);
+        }
+        [HttpPost]
+        //Google Charts Implementation
+        public async Task<JsonResult> GglTicketPriority()
+        {
+            int companyId = User.Identity!.GetCompanyId();
+
+            List<Ticket> tickets = await _ticketService.GetAllTicketsByCompanyAsync(companyId);
+
+            List<object> chartData = new();
+            chartData.Add(new object[] { "Priority", "Count" });
+
+
+            foreach (string priority in Enum.GetNames(typeof(BTTicketPriority)))
+            {
+                int priorityCount = (await _ticketService.GetAllTicketsByPriorityAsync(companyId, priority)).Count();
+                chartData.Add(new object[] { priority, priorityCount });
+            }
+
+            return Json(chartData);
+        }
+        [HttpPost]
+        //Google Charts Implementation
+        public async Task<JsonResult> GglTicketStatus()
+        {
+            int companyId = User.Identity!.GetCompanyId();
+
+            List<Ticket> tickets = await _ticketService.GetAllTicketsByCompanyAsync(companyId);
+
+            List<object> chartData = new();
+            chartData.Add(new object[] { "Status", "Count" });
+
+
+            foreach (string status in Enum.GetNames(typeof(BTTicketStatus)))
+            {
+                int statusCount = (await _ticketService.GetAllTicketsByStatusAsync(companyId, status)).Count();
+                chartData.Add(new object[] { status, statusCount });
+            }
+
+            return Json(chartData);
+        }
+
         public IActionResult Default()
         {
             return View();
